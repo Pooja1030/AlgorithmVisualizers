9bc2a18fba7f4d9fac96f39ccf9d5c497e879e9e
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2023-12-12 02:38:58 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
+import NoPage from "./Pages/NoPage";
 import Sort from './Sorting/sort';
-import Pathfinder from "./Pages/Pathfinder";
+import PathfindingVisualizer from './PathFindingVisualizer/PathFindingVisualizer';
 import RecursiveSort from "./recursivesort/recursiveSort";
 import ConvexHull from "./convexHull/convexHull";
 // import Tree from "./RecursiveGraph/Tree"
 import Graph from './RecursiveGraph/graph';
 import Puzzle from './15Puzzle/puzzle';
 import Queen from './Queen/queen';
-// import Sort from "./Pages/Sort";
-// import Sorting from './Pages/Sorting';
-// import NoPage from "./Pages/NoPage";
 
 
 
@@ -28,16 +26,13 @@ function App() {
             <Route index element={<Home />} />
             <Route path='/sort' Component={Sort} />
             <Route path="Sort" element={<Sort />} />
-            {/* <Route path="Sorting/" element={<Sorting />} /> */}
-            <Route path="pathfinder" element={<Pathfinder />} />
+            <Route path="pathfinder" element={<PathfindingVisualizer />} />
             <Route path="recursiveSort" element={<RecursiveSort />} />
             <Route path="convexHull" element={<ConvexHull />} />
             <Route path="graph" element={<Graph/>} />
             <Route path="puzzle" element={<Puzzle/>} />
             <Route path="queen" element={<Queen/>} />
-
-            {/* <Route path="*" element={<NoPage />} /> */}
-
+            <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/Pages/NoPage.js
+++ src/Pages/NoPage.js
@@ -4,8 +4,10 @@ import errorillustration from '../404-error-illustration.jpg';
 const noPage = () => {
     return (
         <>
-        <img src={errorillustration} alt="404" style={{width:"400px"}}/>
-        <Link to="/"><h1>Go back to home</h1></Link>
+        <div style={{display:"inline-flex",flexDirection:"column", justifyContent:"center"}}>
+        <img src={errorillustration} alt="404" style={{width:"600px"}}/>
+        <button style={{backgroundColor:"#E8FFAF", border:"#1B2E35 1px solid"}}><Link to="/">Go back to home</Link></button>
+        </div>
         <Outlet />
     </>
     );
