46c464faeb871a9158d2b0bacc0ec94c7b3b10ba
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-04-30 02:14:45 +0530

    Queue

--- src/Queue/queue.js
+++ src/Queue/queue.js
@@ -1,59 +1,103 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Components/navbar';
+import DiscreteSlider from '../Components/slider';
+import './queue.css';
 
-class Queue extends Component {
-    constructor() {
-        super();
-        this.state = {
-            queue: [],
-            inputValue: '',
-        };
-    }
+const QueueVisualizer = () => {
+    const [queue, setQueue] = useState([]);
+    const [maxSize, setSize] = useState(5);
+    const [dequeuedElement, setDequeuedElement] = useState(null);
+    const [resultText, setResultText] = useState(null);
+    const [currVal, setCurrVal] = useState(null);
 
-    enqueueToQueue = () => {
-        const { queue, inputValue } = this.state;
-        const updatedQueue = [...queue, inputValue];
-        this.setState({ queue: updatedQueue, inputValue: '' });
-    };
-
-    dequeueFromQueue = () => {
-        const { queue } = this.state;
-        if (queue.length === 0) {
-            alert("Queue is empty!");
-            return;
+    const enqueue = () => {
+        setResultText(null);
+        setCurrVal(null);
+        if (queue.length < maxSize) {
+            const newValue = Math.floor(Math.random() * 10) + 1; // Generate random value
+            setQueue(prevQueue => [...prevQueue, newValue]);
+        } else {
+            setResultText("");
+            setCurrVal('Queue is full');
         }
-        const updatedQueue = [...queue];
-        updatedQueue.shift();
-        this.setState({ queue: updatedQueue });
     };
 
-    handleInputChange = (e) => {
-        this.setState({ inputValue: e.target.value });
+    const dequeue = () => {
+        if (queue.length > 0) {
+            setResultText('Dequeued: ');
+            setCurrVal(queue[queue.length - 1]);
+            setDequeuedElement(queue[0]); // Store the dequeued element
+            setQueue(prevQueue => prevQueue.slice(1)); // Remove the first element from the queue
+            setTimeout(() => {
+                setDequeuedElement(null); // Clear the dequeued element after the animation duration
+            }, 500); // Adjust animation duration as needed
+        } else {
+            setResultText("");
+            setCurrVal('Queue is empty');
+        }
     };
 
-    render() {
-        const { queue, inputValue } = this.state;
 
-        return (
-            <div>
-                <Navbar />
-                <h2>Queue Data Structure</h2>
+    const peek = () => {
+        if (queue.length > 0) {
+            setResultText('Top value: ')
+            setCurrVal(queue[0]); // Peek at the first element in the queue
+        } else {
+            setResultText("");
+            setCurrVal('Queue is empty');
+        }
+    };
+
+    const isEmpty = () => {
+        setResultText('Is empty: ')
+        setCurrVal(queue.length === 0 ? 'True' : 'False');
+    };
+
+    const size = () => {
+        setResultText('Size: ')
+        setCurrVal(queue.length);
+    };
+
+
+    return (
+        <>
+            <Navbar currentPage="Queue" />
+
+            <div className="queue-visualizer">
                 <div>
-                    <input type="text" value={inputValue} onChange={this.handleInputChange} />
-                    <button onClick={this.enqueueToQueue}>Enqueue</button>
-                    <button onClick={this.dequeueFromQueue}>Dequeue</button>
+                    <div className="menu">
+                        <DiscreteSlider
+                            title='Queue size'
+                            defaultValue={maxSize}
+                            onCountChange={setSize}
+                            step={1}
+                            min={1}
+                            max={10} />
+                        <button className='visualize-btn' onClick={enqueue}>Enqueue</button>
+                        <button className='reset-btn' onClick={dequeue}>Pop</button>
+                        <button onClick={peek}>Peek</button>
+                        <button onClick={isEmpty}>IsEmpty</button>
+                        <button onClick={size}>Size</button>
+                    </div>
+                    <div className="result">{currVal !== null && `${resultText} ${currVal}`}</div>
                 </div>
-                <div>
-                    <h3>Queue:</h3>
-                    <ul>
-                        {queue.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))}
-                    </ul>
+
+                <div className="queue">
+                    {dequeuedElement && ( // Render the dequeqed element with animation if it exists
+                        <div className="element dequeued" onAnimationEnd={() => setDequeuedElement(null)}>
+                            {dequeuedElement}
+                        </div>
+                    )}
+                    {queue.map((value, index) => (
+                        <div key={index} className="element">
+                            {value}
+                        </div>
+                    ))}
+
                 </div>
             </div>
-        );
-    }
-}
+        </>
+    );
+};
 
-export default Queue;
+export default QueueVisualizer;
