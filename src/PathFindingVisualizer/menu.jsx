2304a3e09b42ba21147dffb97b965fe78ae508c2
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-05-06 01:34:55 +0530

    pathfinder menu

--- src/PathFindingVisualizer/menu.jsx
+++ src/PathFindingVisualizer/menu.jsx
@@ -9,48 +9,50 @@ import Drawer from '@mui/material/Drawer';
 class Menu extends Component {
     render() {
         return (
-        //     <Drawer
-        //     anchor="left"
-        //     open={this.props.open}
-        //     onClose={this.props.toggleDrawer}
-        // >
-            <nav className="nav menu alert-dark">
+            //     <Drawer
+            //     anchor="left"
+            //     open={this.props.open}
+            //     onClose={this.props.toggleDrawer}
+            // >
+                <nav className="nav menu alert-dark">
 
-                <DiscreteSlider
-                    default={50}
-                    min={10}
-                    max={100}
-                    step={5}
-                    title="Speed"
-                    onCountChange={this.props.onSpeedChange}
-                    isDisabled={false}
-                />
-                <SimpleSelect
-                    pos={0}
-                    onAlgoChanged={this.props.onAlgoChanged}
-                    items={["Dijkstra's Algorithm",
-                        "A* Algorithm", 
-                        "Depth-first Search", 
-                        "Breadth-first search"]} />
-                <div>
-                    <button
-                        className='visualize-btn btn-warning btn-lg '
-                        onClick={this.props.onVisualize}
-                        disabled={this.props.isDisabled}
+                    <SimpleSelect
+                        label={"Algorithm"}
+                        pos={0}
+                        onValueChanged={this.props.onAlgoChanged}
+                        items={["Dijkstra's Algorithm",
+                            "A* Algorithm",
+                            "Depth-first Search",
+                            "Breadth-first search"]} />
+
+                    <DiscreteSlider
+                        default={50}
+                        min={10}
+                        max={100}
+                        step={5}
+                        title="Speed"
+                        onCountChange={this.props.onSpeedChange}
+                        isDisabled={false}
+                    />
+                    <div>
+                        <button
+                            className='visualize-btn btn-warning btn-lg '
+                            onClick={this.props.onVisualize}
+                            disabled={this.props.isDisabled}
                         // style={this.isClickable()}
-                    >
-                        Visualize
-                    </button>
-                    <button
-                        className='reset-btn btn-secondary m-2'
-                        onClick={this.props.onRandomize}
-                        disabled={this.props.isDisabled}
-                        style={this.isClickable()}
-                    >
-                        Reset
-                    </button>
-                </div>
-            </nav>
+                        >
+                            Visualize
+                        </button>
+                        <button
+                            className='reset-btn btn-secondary m-2'
+                            onClick={this.props.onRandomize}
+                            disabled={this.props.isDisabled}
+                            style={this.isClickable()}
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </nav>
             // </Drawer>
         );
     }
