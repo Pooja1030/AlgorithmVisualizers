d9ebbe73a252537794644a6cb5f857d5e2dd976f
Pooja <poojasawant1030@gmail.com>
2024-05-13 18:03:36 +0530

    updated stack sidepanel

--- src/Stack/stack.js
+++ src/Stack/stack.js
@@ -13,7 +13,6 @@ const StackVisualizer = () => {
     const [currVal, setCurrVal] = useState(null);
     const [sidePanelOpen, setSidePanelOpen] = useState(false); // State to manage side panel visibility
     const [algorithmSteps, setAlgorithmSteps] = useState([]); // Define state for algorithm steps
-    // const [isPlaying, setIsPlaying] = useState(false); // State to manage play-pause functionality
 
     useEffect(() => {
         // Define your algorithm steps here
@@ -45,13 +44,13 @@ by TOP and reduce its value.`
         setAlgorithmSteps(steps);
     }, []);
 
-
     const push = () => {
         setResultText(null);
         setCurrVal(null);
         if (stack.length < maxSize) {
             const newValue = Math.floor(Math.random() * 10) + 1; // Generate random value for new die
             setStack(prevStack => [...prevStack, newValue]);
+            triggerSidePanel('Push');
         } else {
             setResultText("");
             setCurrVal('Stack is full');
@@ -67,6 +66,7 @@ by TOP and reduce its value.`
             setTimeout(() => {
                 setPoppedDie(null); // Clear the popped die after the animation duration
             }, 500); // Adjust animation duration as needed
+            triggerSidePanel('Pop');
         } else {
             setResultText("");
             setCurrVal('Stack is empty');
@@ -80,7 +80,6 @@ by TOP and reduce its value.`
             const timeline = gsap.timeline();
             timeline.to(".top", { background: "#992155", duration: 0.5 });
             timeline.to(".top", { background: "#fb21d3", duration: 0.5, delay: 1 });
-
         } else {
             setResultText("");
             setCurrVal('Stack is empty');
@@ -101,30 +100,71 @@ by TOP and reduce its value.`
         setSidePanelOpen(!sidePanelOpen);
     };
 
-    // const togglePlayPause = () => {
-    //     setIsPlaying(!isPlaying);
-    // };
-    // const togglePlayPause = () => {
-    //     setIsPlaying(!isPlaying);
-    //   };
-
+    const triggerSidePanel = (operation) => {
+        setSidePanelOpen(true);
+        switch (operation) {
+            case 'Push':
+                setAlgorithmSteps([
+                    { code: "Push Operation" },
+                    { code: "Push operation includes various steps, which are as follows :" },
+                    { code: "Step 1: First, check whether or not the stack is full" },
+                    { code: "Step 2: If the stack is complete, then exit" },
+                    { code: "Step 3: If not, increment the top by one" },
+                    { code: "Step 4: Insert a new element where the top is pointing" },
+                    { code: "Step 5: Success" }
+                ]);
+                break;
+            case 'Pop':
+                setAlgorithmSteps([
+                    { code: "Pop Operation" },
+                    { code: "Pop operation includes various steps, which are as follows :" },
+                    { code: "Step 1: First, check whether or not the stack is empty" },
+                    { code: "Step 2: If the stack is empty, then exit" },
+                    { code: "Step 3: If not, access the topmost data element" },
+                    { code: "Step 4: Decrement the top by one" },
+                    { code: "Step 5: Success" }
+                ]);
+                break;
+            case 'IsEmpty':
+                setAlgorithmSteps([
+                    { code: "IsEmpty Operation" },
+                    { code: "IsEmpty operation includes various steps, which are as follows :" },
+                    { code: "Step 1: Check if the stack is empty" },
+                    { code: "Step 2: If the stack is empty, return true" },
+                    { code: "Step 3: If not, return false" }
+                ]);
+                break;
+            case 'Peek':
+                setAlgorithmSteps([
+                    { code: "Peek Operation" },
+                    { code: "Peek operation includes various steps, which are as follows :" },
+                    { code: "Step 1: Check if the stack is empty" },
+                    { code: "Step 2: If the stack is empty, display an error message" },
+                    { code: "Step 3: If not, return the top element of the stack" }
+                ]);
+                break;
+            case 'Size':
+                setAlgorithmSteps([
+                    { code: "Size Operation" },
+                    { code: "Size operation includes various steps, which are as follows :" },
+                    { code: "Step 1: Get the number of elements in the stack" },
+                    { code: "Step 2: Return the count of elements" }
+                ]);
+                break;
+            default:
+                
+                break;
+        }
+    };
+    
 
     return (
         <>
             <Navbar currentPage="Stack" />
-
             {/* Side panel toggle button */}
             <button className="side-panel-toggle" onClick={toggleSidePanel}>→</button>
-
-            {/* Render the play-pause buttons */}
-            {/* <button className="play-pause-btn" onClick={togglePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button> */}
-            {/* <button className="play-pause-btn" onClick={togglePlayPause}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button> */}
-
             {/* Render the side panel component */}
             <SidePanel algorithmSteps={algorithmSteps} isOpen={sidePanelOpen} onClose={toggleSidePanel} />
-
             <div className="stack-visualizer">
                 <div>
                     <div className="menu">
@@ -155,54 +195,6 @@ by TOP and reduce its value.`
                             {poppedDie}
                         </div>
                     )}
-                </div>
-
-                <div className="representation">
-                    <div class="row mx-auto" id="pop-pseudocode">
-                        <div class="col-sm-12 col-md-12 col-lg-4 px-0 mr-0">
-                            <div class="ide w-100">
-                                <div class="row ml-auto mr-auto 1">
-                                    <h3>PUSH</h3>
-                                    <span class="comment w-100 mt-1"> Add an element to the top of a stack</span>
-                                    <span class="comment w-100 mt-1"> </span>
-                                    <span class="comment w-100 mt-1">TIME COMPLEXITY: O(1)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-12 col-lg-4 px-0 mr-0">
-                            <div class="ide w-100">
-                                <div class="row ml-auto mr-auto 1">
-                                    <h3>POP</h3>
-                                    <span class="comment w-100 mt-1"> Remove an element from the top of a stack</span>
-                                    <span class="comment w-100 mt-1"> </span>
-                                    <span class="comment w-100 mt-1">TIME COMPLEXITY: O(1)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-12 col-lg-4 px-0 mr-0">
-                            <div class="ide w-100">
-                                <div class="row ml-auto mr-auto 1">
-
-                                    <h3> PEEK</h3>
-                                    <span class="comment w-100 mt-1">Get the value of the top element without removing it</span>
-                                    <span class="comment w-100 mt-1"> </span>
-                                    <span class="comment w-100 mt-1">TIME COMPLEXITY: O(1)</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="explanation">
-                        <h3>Explanation</h3>
-                        <p>
-                            The stack data structure follows the Last-In-First-Out (LIFO) principle.
-                            Elements are added and removed from only one end, the top.
-                            The push operation inserts a new element onto the top of the stack.
-                            The pop operation removes and returns the top element from the stack.
-                            The peek operation returns the value of the top element without removing it.
-                            The isEmpty operation checks if the stack is empty.
-                            The size operation returns the number of elements currently in the stack.
-                        </p>
-                    </div> */}
                 </div>
             </div>
         </>
