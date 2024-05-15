4f16d5a8c2b119ae34630494b2e082d93f982e2c
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-05-16 01:25:49 +0530

    MST sidepannel & complexities

--- src/MST/mst.js
+++ src/MST/mst.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "../Components/navbar";
 import DiscreteSlider from '../Components/slider';
+import SidePanel from '../Components/sidepanel';
 import Canvas from './canvas'; // Assuming you have a Canvas component for drawing
 import kruskal from './kruskal';
 import prim from './prim';
@@ -16,10 +17,15 @@ class MST extends Component {
 			mstEdges: [],
 			maxWidth: 700,
 			maxHeight: 500,
-			algorithm: 'kruskal', // Default algorithm
+			algorithm: '', // Default algorithm
 			maxVertices: 5,
 			animationSpeed: 50,
-			isAnimating: false // Flag to track animation state
+			isAnimating: false, // Flag to track animation state
+
+			sidePanelOpen: false,
+			algorithmSteps: [],
+			timeComplexity: "",
+			spaceComplexity: "",
 		};
 		this.animationTimeline = gsap.timeline({ paused: true }); // Initialize GSAP timeline
 	}
@@ -27,12 +33,19 @@ class MST extends Component {
 	componentDidMount() {
 		// Initialize your graph data, generate vertices, etc.
 		this.generateVertices();
+		// const { algorithm } = this.state;
+	
 		// this.calculateMST();
 	}
 
 	handleAlgorithmChange = (event) => {
-		this.setState({ algorithm: event.target.value });
-		this.setState({ visitedEdges: [], mstEdges: [] }); // Clear vertices and MST edges
+		const algorithm = event.target.value;
+		this.setState({ algorithm, visitedEdges: [], mstEdges: [] }); // Clear vertices and MST edges
+		this.setAlgoSteps(algorithm);
+		const timeComplexity = this.calculateTimeComplexity(algorithm);
+		const spaceComplexity = this.calculateSpaceComplexity(algorithm);
+		this.setState({ timeComplexity, spaceComplexity });
+	
 	}
 
 	// Define the method to set the animation speed
@@ -82,8 +95,77 @@ class MST extends Component {
 		this.setState({ mstEdges, visitedEdges });
 	}
 
+	visualize() {
+		this.calculateMST()
+		this.setState({ sidePanelOpen: true });
+	}
+
+	setAlgoSteps = (algorithm) => {
+		let algorithmSteps = [];
+		if (algorithm === "kruskal") {
+			algorithmSteps = [
+				{ code: " Step 1: Sort all the edges by weight (non-decreasing)." },
+				{ code: " Step 2: Pick the edge with the smallest weight." },
+				{ code: " Step 3: Check if adding that edge to the MST forms a cycle. If it doesn't, add it." },
+				{ code: " Step 4: Repeat steps 2 and 3 until the MST has a length of (number of Vertices - 1)." }
+				// Add more steps as needed
+			];
+		} else if (algorithm === "prim") {
+			algorithmSteps = [
+				{ code: " Step 1: Initialize the start node and the open set." },
+				{ code: " Step 2: Loop until the open set is empty." },
+				{ code: " Step 3: Select the node with the lowest f score from the open set." },
+				{ code: " Step 4: If the selected node is the finish node, reconstruct the path." },
+				{ code: " Step 5: Generate the neighbors of the selected node." },
+				{ code: " Step 6: For each neighbor, calculate tentative g score and add it to the open set." },
+				{ code: " Step 7: Repeat the loop." }
+				// Add more steps as needed
+			];
+		}
+		this.setState({ algorithmSteps });
+	}
+
+	// Function to calculate time complexity based on the selected algorithm
+	calculateTimeComplexity(algorithm) {
+		let timeComplexity = "";
+		switch (algorithm) {
+			case "prim":
+				timeComplexity = "O(V^2)"; // Real time complexity of Prim's Algorithm
+				break;
+			case "kruskal":
+				timeComplexity = "O(E log V)"; // Real time complexity of Kruskal's Algorithm
+				break;
+			default:
+				timeComplexity = "";
+		}
+		return timeComplexity;
+	}
+
+	// Function to calculate space complexity based on the selected algorithm
+	calculateSpaceComplexity(algorithm) {
+		let spaceComplexity = "";
+		switch (algorithm) {
+			case "prim":
+				spaceComplexity = "O(V)"; // Real space complexity of Prim's Algorithm
+				break;
+			case "kruskal":
+				spaceComplexity = "O(E + V)"; // Real space complexity of Kruskal's Algorithm
+				break;
+			default:
+				spaceComplexity = "";
+		}
+		return spaceComplexity;
+	}
+
+	toggleSidePanel = () => {
+		this.setState((prevState) => ({ sidePanelOpen: !prevState.sidePanelOpen }));
+	};
+
+
 	render() {
 		const { vertices, edges, visitedEdges, mstEdges, maxHeight, maxWidth, algorithm, animationSpeed } = this.state;
+		const { sidePanelOpen, algorithmSteps, timeComplexity, spaceComplexity } = this.state;
+
 		return (
 			<div>
 				<Navbar currentPage="Minimum Spanning Tree" />
@@ -111,13 +193,31 @@ class MST extends Component {
 					/>
 
 					<div>
-						<button className='visualize-btn' onClick={() => this.calculateMST()}>Visualize</button>
+						<button className='visualize-btn' onClick={() => this.visualize()}>Visualize</button>
 						<button className='reset-btn' onClick={() => this.generateVertices()}>Reset</button>
 					</div>
 				</div>
+
+				<button className="side-panel-toggle" onClick={this.toggleSidePanel}>
+					→
+				</button>
+
+				<SidePanel algorithmSteps={algorithmSteps} isOpen={sidePanelOpen} onClose={this.toggleSidePanel} />
+
 				{/* Render any UI elements here */}
 				<Canvas vertices={vertices} mstEdges={mstEdges} connectingEdges={edges} visitedEdges={visitedEdges}
-					width={maxWidth} height={maxHeight} speed={animationSpeed}/>
+					width={maxWidth} height={maxHeight} speed={animationSpeed} />
+
+
+				{algorithm && (
+					<div className="complexity-analysis">
+						<div className="analysis-title">Time Complexity:</div>
+						<div className="analysis-result">{timeComplexity !== null ? `${timeComplexity} ms` : "Not measured"}</div>
+						<div className="analysis-title">Space Complexity:</div>
+						<div className="analysis-result">{spaceComplexity !== null ? `O(${spaceComplexity})` : "Not measured"}</div>
+					</div>
+
+				)}
 
 			</div>
 		);
