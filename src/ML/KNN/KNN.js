481a33c2971e22b31ffdedf89ff97515969e8ccf
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-06-12 07:41:21 +0530

    updated knn

--- src/ML/KNN/KNN.js
+++ src/ML/KNN/KNN.js
@@ -85,7 +85,7 @@ function KNN() {
     if (data) {
       const svg = d3.select(svgRef.current)
         .attr('width', 800)
-        .attr('height', 400);
+        .attr('height', 600);
 
       const margin = { top: 20, right: 30, bottom: 60, left: 40 };
       const width = +svg.attr('width') - margin.left - margin.right;
@@ -119,7 +119,7 @@ function KNN() {
 
       svg.append('text')
         .attr('x', width / 2)
-        .attr('y', height + margin.bottom / 2)
+        .attr('y', height + margin.bottom / 2 - 30)
         .attr('text-anchor', 'middle')
         .text('Feature 1');
 
@@ -141,6 +141,13 @@ function KNN() {
     }
   }, [data]);
 
+  const features = [
+    "Sepal length (cm)",
+    "Sepal width (cm)",
+    "Petal length (cm)",
+    "Petal width (cm)",
+  ]
+
   return (
     <div>
       <Navbar currentPage="K-Nearest Neighbours"
@@ -148,7 +155,7 @@ function KNN() {
       <div className='menu'>
         {input.map((val, index) => (
           <div key={index}>
-            <label>Feature {index + 1}:</label>
+            <label>{features[index]}:</label>
             <input
               type="number"
               value={val}
@@ -156,11 +163,35 @@ function KNN() {
             />
           </div>
         ))}
-        <button onClick={handlePredict}>Predict</button>
+        <button className='visualize-btn' onClick={handlePredict}>Predict</button>
       </div>
       {prediction && <div className='result'>Prediction: {prediction}</div>}
 
-      <svg ref={svgRef} id="chart"></svg>
+      <div className='regression'>
+        <div className="graph-container">
+          <div className='graph' style={{ padding: "10px 30px 30px 10px", height: "550px", width: "750px" }}>
+            <svg ref={svgRef} id="chart"></svg>
+          </div>
+        </div>
+      </div>
+
+      <div className="info-container-2">
+        <div className="info-section">  <h3>Iris Dataset Details</h3>
+          <p>The Iris dataset is a classic dataset in machine learning and statistics. It includes 150 observations of iris flowers, each described by four features:</p>
+          <ul>
+            <li>Sepal length (cm)</li>
+            <li>Sepal width (cm)</li>
+            <li>Petal length (cm)</li>
+            <li>Petal width (cm)</li>
+          </ul>
+          <p>Each observation belongs to one of three species of iris:</p>
+          <ul>
+            <li>Iris setosa</li>
+            <li>Iris versicolor</li>
+            <li>Iris virginica</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
